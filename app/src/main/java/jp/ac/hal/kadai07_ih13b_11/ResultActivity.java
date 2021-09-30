4dd28f4f1106f3d30b1f47da28e5423d14a38f69
THS95049 <THS95049@DESKTOP-IG0TADM>
2021-09-30 14:07:30 +0000

    :lipstick: ui: Create result activity

---
+++ app/src/main/java/jp/ac/hal/kadai07_ih13b_11/ResultActivity.java
@@ -0,0 +1,16 @@
+package jp.ac.hal.kadai07_ih13b_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
