e131e6dca7b13961ae25cf953bad7566fefc8c18
THS95049 <THS95049@DESKTOP-IG0TADM>
2021-09-30 07:54:52 +0000

    :lipstick: ui: Create ui for intro activity

--- app/src/main/java/jp/ac/hal/kadai07_ih13b_11/IntroActivity.java
+++ app/src/main/java/jp/ac/hal/kadai07_ih13b_11/IntroActivity.java
@@ -1,14 +1,14 @@
 package jp.ac.hal.kadai07_ih13b_11;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 }
